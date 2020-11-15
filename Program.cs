3e626899c8f5a7bb77eb13a5e6a438a13647896b
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-11-15 20:55:10 -0300

    Resolução do Exercício

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == 0)
+            {
+                Console.WriteLine("Zero");
+                Environment.Exit(0);
+            }
+
+            if (numero > -1)
+            {
+                Console.WriteLine("Positivo");
+            }
+            else
+            {
+                Console.WriteLine("Negativo");
+            }
+
+        }
+    }
+}
